f1eb26f5164786c4432d6eb66cd8874b43e750cb
SebasRojas <sevassrojas2603@gmail.com>
2025-06-07 09:45:46 -0300

    Cree la pagina de inicio

--- motor-admin/PAGES/inicio.php
+++ motor-admin/PAGES/inicio.php
@@ -4,16 +4,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MotorAdmin</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9Oer+0wPthOhA8rsVjQerV_D3B3z_oB-4o5uG0i3F_M4hK2f" crossorigin="anonymous">
-    <link rel="stylesheet" href="../CSS/turnos.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="../CSS/inicio.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css" rel="stylesheet">
 </head>
 <body>
     <?php
       include("../UTILS/header-pages.php");
     ?>
+    <section class="historia-horizontal">
+      <div class="historia-texto">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam non excepturi quis officiis voluptates...</p>
+      </div>
+      <div class="historia-imagen">
+        <img src="../images/historia-taller.jpeg" alt="fotoAuto">
+      </div>
+    </section>
     
-    
+    <section id="historia">
+      <div class="parallax-img">
+        <img src="../images/quienes-somos.jpg" alt="fotoNosotros">
+      </div>
+      <div class="parallax-text">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus, 
+        nemo adipisci! Voluptas nobis fugit vero dicta commodi nulla consequuntur! Corrupti doloremque iste voluptatem culpa, ad omnis! Eius earum laborum voluptatum.</p>
+      </div>
+    </section>
+
+    <section id="turnos">
+      <div class="turnos-img">
+        <img src="../images/mision-vision.jpg" alt="fotoCalendario">
+      </div>
+      <div class="turnos-text">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur in minus at molestiae doloremque fuga, blanditiis consectetur, illo recusandae enim optio harum non doloribus ullam quas nihil quidem suscipit aliquid?</p>
+        <button>Reserva tu turno</button>
+      </div>
+    </section>
+
+    <section id="servicios">
+      <p>Nuestros Servicios</p>
+      <div class="servicios-fila">
+        <div class="servicio"><i class="ri-oil-line"></i><p>Chequeo y cambio de aceite, cambio liquido refrigerantea, rellenar el sapito</p></div>
+        <div class="servicio"><i class="ri-tools-fill"></i><p>Cambio de ruedas, cambio de frenos</p></div>
+        <div class="servicio"><i class="ri-steering-fill"></i><p>Dirección y alineacion, ajuste de balanceo</p></div>
+        <div class="servicio"><i class="ri-car-fill"></i><p>Chequeos generales</p></div>
+      </div>
+    </section>
+
+    <section id="contacto">
+      <div class="contacto-img">
+        <img src="../images/mision-vision.jpg" alt="fotoMaps">
+      </div>
+      <div class="contacto-text">
+        <a href="www.googlemaps.com">googlemaps</a>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur accusamus ducimus asperiores mollitia, veniam vel cumque suscipit praesentium, veritatis alias perspiciatis similique nihil nemo pariatur blanditiis et laboriosam commodi quasi!</p>
+        <button>Contactanos</button>
+      </div>
+    </section>
+
     <?php
       include("../UTILS/footer.php");
     ?>
+    <script src="../JS/inicio.js"></script>
 </body>
--- motor-admin/main.js
+++ motor-admin/main.js
@@ -1,8 +1,12 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hola mundo!</h1>
-  </div>
-`
+window.addEventListener('scroll', () => {
+  const img = document.querySelector('.parallax-img');
+  const text = document.querySelector('.parallax-text');
+
+  const offset = window.scrollY;
+
+  img.style.transform = `translateY(${offset * 0.3}px)`;
+  text.style.transform = `translateY(${offset * 0.15}px)`;
+});
 
